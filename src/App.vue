2a2e0d1aa1f3b86e41d8fdba922ffb9d0ba595ab
mohamedelgendi <mohelgendi@hotmail.com>
2020-03-09 14:29:21 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,32 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>
+      |
       <router-link to="/about">About</router-link>
+      |
+      <router-link to="/login">Login</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
     color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
   }
-}
+  html, body {
+    height: 100%;
+  }
+  body.login {
+    background-image: linear-gradient(to right top, #220083, #2c2a9a, #3a49af, #4c65c2, #6282d3, #5f97e2, #61abee, #69bff8, #52cffe, #41dfff, #46eefa, #5ffbf1);
+    -moz-background-size: cover;
+    -webkit-background-size: cover;
+    -o-background-size: cover;
+    background-size: contain;
+  }
 </style>
---
+++ src/views/Login.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="login-page">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  beforeCreate: function () {
+    document.body.className = 'login'
+  },
+  beforeDestroy () {
+    document.body.className = ''
+  }
+}
+</script>
+
+<style scoped>
+#login-page {
+  background-image: linear-gradient(to right bottom, #22128e, #23289d, #263bab, #2a4cb9, #315dc6, #1e72d6, #0586e3, #009aef, #00b5fa, #00cefc, #00e5f8, #5ffbf1);
+}
+</style>
